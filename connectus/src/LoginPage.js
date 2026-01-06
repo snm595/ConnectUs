5f900bb285d1763ea47f0a91fefb6c5615ef67ec
snm595 <152716749+snm595@users.noreply.github.com>
2026-01-06 15:15:35 +0530

    Update LoginPage.js

--- connectus/src/LoginPage.js
+++ connectus/src/LoginPage.js
@@ -34,11 +34,15 @@ const LoginPage = ({ onAuthenticate }) => {
       return;
     }
     try {
-      const response = await fetch('http://localhost:5000/api/auth/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ email, password, role })
-      });
+          const response = await fetch(
+              `${process.env.REACT_APP_API_URL}/api/auth/login`,
+          {
+              method: 'POST',
+              headers: { 'Content-Type': 'application/json' },
+              body: JSON.stringify({ email, password, role })
+          }
+      );
+
       const data = await response.json();
       if (response.ok) {
         // Store userId from backend response
